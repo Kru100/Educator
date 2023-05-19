a03576075ace05cf8cb7fc98b09368b3d4ef3630
Krunal <@kp1584085@gmail.com>
2023-05-19 16:50:49 +0530

    URL problem has been fixed

--- authentic/apps.py
+++ authentic/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AuthenticationModuleConfig(AppConfig):
+class AuthenticConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'authentication_module'
+    name = 'authentic'
--- authentication_module/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from authentication_module import views
-
-urlpatterns = [
-    path('signup/',views.signup, name='signup')
-]
