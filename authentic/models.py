a8c2b09ceb28257992273f58411165e9883fb7c4
Krunal <@kp1584085@gmail.com>
2023-05-22 11:39:39 +0530

    Created models Instructor, Admin and subject

--- authentic/models.py
+++ authentic/models.py
@@ -1,5 +1,13 @@
 from djongo import models
 
+class subject(models.Model):
+    name = models.CharField(max_length=50)
+    instructor_username = models.CharField(max_length=50)
+    is_allowed = models.BooleanField(default=False)
+    duration = models.IntegerField()
+    description = models.TextField()
+    is_active = models.BooleanField(default=False)
+        
 class students(models.Model):
     username = models.CharField(max_length=50)
     name = models.CharField(max_length=100)
@@ -7,5 +15,19 @@ class students(models.Model):
     is_registered = models.BooleanField(default=False)
     is_verified = models.BooleanField(default=False)
     password = models.CharField(max_length=16)
+    #subject = models.ArrayField(model_container=subject)
+
+class Instructor(models.Model):
+    username = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    qualification = models.TextField()
+    is_registered = models.BooleanField(default=False)
+    is_verified = models.BooleanField(default=False)
+    password = models.CharField(max_length=16)
+    #subject = models.ArrayField(models.CharField(max_length=24),size=3)
     
+class Admin(models.Model):
+    password = models.CharField(max_length=16)
+
     
